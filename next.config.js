474fe4357c955729c12c7b0c9da0177fc66f5819
sahildotexe <sahilkaling@gmail.com>
2023-03-03 03:22:35 +0530

    final fix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true
+ },
+ exportTrailingSlash:true,
+ exportPathMap: function() {
+  return {
+    '/':{page: '/'}
+  };
  }
 }
 
